0b2b5b7553c4a78a2b9a52dfa95284713f389eed
AlvaroLealGei <alvaro.leal@tupad.utn.edu.ar>
2026-05-22 13:38:20 -0300

    cree Tp7_datosComplejos
    Creacion nueva carpeta y nuevo archivo

---
+++ 06 Datos Complejos/Tp7_DatosComplejos.py
@@ -0,0 +1,33 @@
+""" #Ejercicio 1
+precio_frutas={
+    'Banana': 1200,
+    'Anana': 2500,
+    'Melon': 3000,
+    'Uva': 1450
+}
+print(precio_frutas)
+precio_frutas.update({'Naranja': 1200, 'Manzana': 1500, 'Pera': 2300})
+print(precio_frutas)
+
+#Ejercicio 2
+precio_frutas['Banana']=1330
+precio_frutas['Manzana']=1700
+precio_frutas['Melon']=2800
+print(precio_frutas)
+
+#Ejercicio 3
+claves=precio_frutas.keys()
+lista_claves= list(claves)
+print(lista_claves)
+#Ejercicio 4
+ """
+agenda={}
+for i in range(2):
+    nombre=input(f"Ingresa el {i+1}° nombre: ")
+    telefono=input(f"Ingresa el numero de la persona n° {i+1}: ")
+    agenda[nombre]= telefono
+busqueda=input("Ingresa un nombre para buscar: ")    
+if busqueda in agenda:
+    print(f"el numero de {busqueda} es: {agenda[busqueda]}")
+else: 
+    print(f"{busqueda} no existe.")
